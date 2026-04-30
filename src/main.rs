650a32f65f4bd2e6cbae9d29a3235a6b874f59b4
toffeantyri <toffeantyri@gmail.com>
2026-04-30 17:40:13 +0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::io;
+mod event;
+use event::Event;
+
+fn main() {
+    test1();
+}
+
+fn test1() {
+    let title = format!("{}, {}", "Угадай", "число");
+    let is_true = true;
+    if !is_true {
+        println!("true");
+        counting(3);
+    } else {
+        println!("false");
+        event_list();
+    }
+
+    println!("{title}");
+    println!("Введите {}", "число: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to readline");
+
+    println!("Ваше число : {guess}");
+}
+
+fn event_list() {
+    let events = vec![
+        Event {
+            year: 2006,
+            desc: "Начал создаваться",
+            title: String::from("Title1"),
+        },
+        Event {
+            year: 2009,
+            desc: "получил спонсирование в Mozzila",
+            title: String::from("Title2"),
+        },
+        Event {
+            year: 2010,
+            desc: "был официально представлен на Mozzila Summit 2010",
+            title: String::from("Title3"),
+        },
+        Event {
+            year: 2012,
+            desc: "выпущена официальная версия Rust 0.1",
+            title: String::from("Title4"),
+        },
+        Event {
+            year: 2015,
+            desc: "вышла первая стабильная версия 1.0",
+            title: String::from("Title5"),
+        },
+    ];
+
+    for e in events {
+        // println!("{:?}", e);
+        println!("{}: {}", e.year, e.desc);
+        println!("{}", std::mem::size_of_val(e.desc));
+    }
+}
+
+fn counting(a: i32) {
+    let mut a1 = 0;
+    while a1 < a {
+        println!("{a1}");
+        a1 += 1;
+    }
+}
