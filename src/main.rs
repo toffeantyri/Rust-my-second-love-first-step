44f01d5f57a7e1d81c4f96239eed849747e23c69
toffeantyri <toffeantyri@gmail.com>
2026-05-21 17:28:19 +0500

    check file is exist

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ use std::fs;
 use std::io::{self, Write};
 
 fn main() {
-    read_from_file();
+    check_file_exist();
 }
 
+fn check_file_exist() {
+    let path = "example_file.txt";
+
+    if fs::metadata(path).is_ok() {
+        println!("File {} is exist ", path);
+    } else {
+        println!("File {} is not exist", path);
+    }
+}
+
+#[allow(dead_code)]
 fn read_from_file() -> io::Result<()> {
     let content = fs::read_to_string("example_file.txt")?;
     println!("{}", content);
     Ok(())
 }
 
+#[allow(dead_code)]
 fn create_write_file() -> io::Result<()> {
     let mut file = fs::File::create("example_file.txt")?;
 
